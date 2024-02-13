8b3a0618ea935a6332074f7eefb62501ca031bcb
aberaslop <berasateui.lopez@gmail.com>
2024-02-13 13:59:10 +0100

    update reads

--- assembly/reads/fastqc.sh
+++ assembly/reads/fastqc.sh
@@ -6,8 +6,30 @@
 #$ -j y
 #$ -t 1-5
 
-STRAIN=$(sed -n ${SGE_TASK_ID}p ../strains)
+#!/bin/bash -l
 
-module load fastqc
+#SBATCH -J trimm                   # Give your job a name, so you can recognize it in the queue overview
+#SBATCH -N 1                       # Define, how many nodes you need. Here, we ask for 1 node.
+#SBATCH -n 4                       # Request 4 cores
+#SBATCH --workdir=/scistor/guest/zrs382/fusarium/genomes/pacbio   # i believe also  works as -D, but leaving it off also works, as it is hte default in slurm.
+#SBATCH --tasks=1
+#SBATCH --cpus-per-task=32
+#SBATCH -p defq
+#SBATCH --output=%x_%j.out         # to name output log : name of name_of_job plus slurm_jobid- but i could do (job array index plus job id = %a_%A.out)
+#SBATCH --error=%x_%j.err          # to name error log
+#SBATCH --mail-type=BEGIN,END,FAIL # Turn on mail notification.  Options NONE, BEGIN, END, FAIL, ALL.
+#SBATCH --mail-user=a.berasateguilopez@vu.nl # to set email address
+#SBATCH --array=1-9                # number of jobs in the array, (the number of genomes)
+#SBATCH --time=0-48:00:00          # REquests 48h runtime reserves the computing resources for 0 days, 4 hours and 0 minutes max (actual time may be shorter if your run completes before this time wall)
+#SBATCH --mem=250G
 
-fastqc -t ${NSLOTS} ${STRAIN}_1_trimmedpaired.fastq.gz ${STRAIN}_2_trimmedpaired.fastq.gz
+
+#load necessary packages
+
+module load shared 2021 FastQC
+
+# execute commands
+
+GENOME=$(sed -n ${SLURM_ARRAY_TASK_ID}p ../genomes)
+
+fastqc -t ${SLURM_NPROCS} ${GENOME}_1_trimmedpaired.fastq.gz ${GENOME}_2_trimmedpaired.fastq.gz
--- assembly/reads/trimmomatic.sh
+++ assembly/reads/trimmomatic.sh
@@ -1,12 +1,45 @@
-#!/bin/sh
-#$ -cwd           
-#$ -pe smp 4      
-#$ -l h_rt=01:00:00 
-#$ -l h_vmem=2G   
-#$ -t 1-5
+#!/bin/bash -l
 
-STRAIN=$(sed -n ${SGE_TASK_ID}p ../strains)
+#SBATCH -J trimm                   # Give your job a name, so you can recognize it in the queue overview
+#SBATCH -N 1                       # Define, how many nodes you need. Here, we ask for 1 node.
+#SBATCH -n 4			   # number of cpus
+#SBATCH --workdir=/scistor/guest/zrs382/fusarium/genomes/pacbio   # i believe also  works as -D, but leaving it off also works, as it is hte default in slurm.
+#SBATCH --tasks=1
+#SBATCH --cpus-per-task=32
+#SBATCH -p defq
+#SBATCH --output=%x_%j.out         # to name output log : name of name_of_job plus slurm_jobid- but i could do (job array index plus job id = %a_%A.out)
+#SBATCH --error=%x_%j.err          # to name error log
+#SBATCH --mail-type=BEGIN,END,FAIL # Turn on mail notification.  Options NONE, BEGIN, END, FAIL, ALL.
+#SBATCH --mail-user=a.berasateguilopez@vu.nl # to set email address
+#SBATCH --array=1-9 	           # number of jobs in the array, (the number of genomes)
+#SBATCH --time=0-04:00:00          #reserves the computing resources for 0 days, 4 hours and 0 minutes max (actual time may be shorter if your run completes before this time wall)
+#SBATCH --mem=250G		   # memory size (per cpu, i think)
 
-module load trimmomatic
+    # You may not place any commands before the last SBATCH directive
+    # Note: for parallel operations increase cpus-per-task above
+    # Note 2: output and error logs can be given absolute paths
 
-java -jar /share/apps/centos7/trimmomatic/0.36/trimmomatic-0.36.jar PE -threads ${NSLOTS} -trimlog fusotu${STRAIN}-trimmomatic.log FUS_OTU${STRAIN}_1.fastq.gz FUS_OTU${STRAIN}_2.fastq.gz FUS_OTU${STRAIN}_1_trimmedpaired.fastq.gz FUS_OTU${STRAIN}_1_trimmedunpaired.fastq.gz FUS_OTU${STRAIN}_2_trimmedpaired.fastq.gz FUS_OTU${STRAIN}_2_trimmedunpaired.fastq.gz ILLUMINACLIP:TruSeq3-PE.fa:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36
+    echo "== Starting run at $(date)"
+    echo "== Job ID: ${SLURM_JOBID}"
+    echo "== Node list: ${SLURM_NODELIST}"
+    echo "== Submit dir. : ${SLURM_SUBMIT_DIR}"
+    echo "== Scratch dir. : ${TMPDIR}"
+
+    # cd $TMPDIR
+    # or change to a project folder with matlab file e.g. hello_World.m
+
+    #if you want to direct to a folder do here
+
+#cd /scistor/guest/zrs382/gallerucinae/
+
+#load necessary packages
+
+module load shared 2021
+conda activate transcriptomics
+
+
+
+GENOME=$(sed -n ${SLURM_ARRAY_TASK_ID}p ../genomes)
+
+
+trimmomatic PE -threads ${SLURM_NPROCS} -trimlog ${GENOME}-trimmomatic.log ${GENOME}_1.fastq.gz ${GENOME}_2.fastq.gz ${GENOME}_1_trimmedpaired.fastq.gz ${GENOME}_1_trimmedunpaired.fastq.gz ${GENOME}_2_trimmedpaired.fastq.gz ${GENOME}_2_trimmedunpaired.fastq.gz ILLUMINACLIP:TruSeq3-PE.fa:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36
